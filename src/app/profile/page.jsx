54dd25dc41b3c39ad474ae536974588cd2550ea3
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-01-24 17:36:23 +0600

    adding something to check

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -14,7 +14,7 @@ const Profile = async () => {
 
     return (
         <div className='flex items-center justify-center md:my-20 my-10 px-6'>
-            <h1 className='lg:text-5xl md:text-4xl text-3xl'>Welcome to your profile {user.given_name + " " + user.family_name}</h1>
+            <h1 className='lg:text-5xl md:text-4xl text-3xl'>Welcome to profile {user.given_name + " " + user.family_name}</h1>
         </div>
 
     );
